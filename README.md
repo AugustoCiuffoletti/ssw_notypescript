956d7ab8c6ff4d1b759fd1ed33a55cf73277bcbd
AugustoCiuffoletti <augusto@di.unipi.it>
2021-03-23 15:37:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-jfmwnv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-jfmwnv)
---
+++ index.js
@@ -0,0 +1,14 @@
+class miaClasse {
+    f1;
+    f2;
+    constructor( a, b ) {
+        this.f1=a,
+        this.f2=b
+    }
+};
+var A=[];
+var n;
+for (n = 0; n < 3 ; n++ ) {
+    A[n] = new miaClasse("",n) ;
+}
+console.log(A)
